c2157d827ffa6d372af6998de9108b3bdb887d48
farehahalias <rehachan40@gmail.com>
2015-10-12 03:28:21 -0400

    setget Python server

--- serverpy.py
+++ serverpy.py
@@ -1,19 +1,20 @@
 import socket
-from socket import*
+from socket import *
 server_socket = socket(AF_INET,SOCK_STREAM)
 server_name = 'localhost'
 server_port = 1026
 server_socket.bind((server_name, server_port))
 server_socket.listen(5)
 get = ''
+get2= ''
 while True:
     c , address = server_socket.accept()
     print "WE GOT CONNECTION FROM",address
     c.send("hello,how are you")
-   get = server_socket.getsockopt(SOL_SOCKET, SO_RCVLOWAT)
-   print "GET RCVLOWAT : ",get
-   set = server_socket.setsockopt(SOL_SOCKET,SO_RCVLOWAT,1)
-   print "SET RCVLOWAT : ",server_socket.getsockopt(SOL_SOCKET,SO_RCVLOWAT)
+    get = server_socket.getsockopt(SOL_SOCKET,SO_SNDBUF)
+    print "GET SNDBUF : ",get
+    set = server_socket.setsockopt(SOL_SOCKET,SO_SNDBUF,1)
+    print "SET SNDBUF : ",server_socket.getsockopt(SOL_SOCKET,SO_SNDBUF)
+    c.close()
+    break;
 
-c.close()
-break;
